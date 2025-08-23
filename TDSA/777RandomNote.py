7faf4bdb4d9ca16ffad51d641531103ff13e3a76
John <jaricaya@gmail.com>
2025-08-23 14:13:13 +0200

    added cpp notes

--- TDSA/777RandomNote.py
+++ TDSA/777RandomNote.py
@@ -585,3 +585,29 @@ class Grade:
 
 #attribute
   minimum_passing = 65
+
+#adding grade using class
+"""
+class Student:
+  def __init__(self, name, year):
+    self.name = name
+    self.year = year
+    self.grades = []
+  def add_grade(self, grade):
+    if isinstance(grade, Grade):
+      self.grades.append(grade)
+
+roger = Student("Roger van der Weyden", 10)
+sandro = Student("Sandro Botticelli", 12)
+pieter = Student("Pieter Bruegel the Elder", 8)
+
+class Grade:
+  def __init__(self, score):
+    self.score = score
+
+  minimum_passing = 65
+
+pieter_grade = Grade(100)
+
+pieter.add_grade(pieter_grade)
+"""
